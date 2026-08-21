d121d3dee3322afa1dbd68cfe46e36f143f6343e
dstepanov <denis.s.stepanov@oracle.com>
2026-08-21 13:45:17 +0200

    ci: raise lighthouse warning thresholds

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -52,10 +52,10 @@ module.exports = {
     },
     assert: {
       assertions: {
-        "categories:performance": ["warn", { minScore: 0.6 }],
+        "categories:performance": ["warn", { minScore: 0.65 }],
         "categories:accessibility": ["error", { minScore: 0.9 }],
-        "categories:best-practices": ["warn", { minScore: 0.8 }],
-        "categories:seo": ["warn", { minScore: 0.8 }],
+        "categories:best-practices": ["warn", { minScore: 0.95 }],
+        "categories:seo": ["warn", { minScore: 0.9 }],
       },
     },
     upload: {
